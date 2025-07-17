9d0994d7c32e67ae013d1b571e0331bef71dcdac
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2025-07-17 15:12:26 +0900

    [level 1] Title: 시저 암호, Time: 2.14 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,10 @@
+def solution(s, n):
+    result = ''
+    for c in s:
+        if c.isupper():  
+            result += chr((ord(c) - ord('A') + n) % 26 + ord('A'))
+        elif c.islower():
+            result += chr((ord(c) - ord('a') + n) % 26 + ord('a'))
+        else: 
+            result += c
+    return result
